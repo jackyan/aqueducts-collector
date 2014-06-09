4fd836198f5cb8cace76c32418fca7dacd42cabb
qudongfang <qudongfang@gmail.com>
2014-06-09 16:23:22 +0800

    add tcp interface suport

--- app.js
+++ app.js
@@ -9,19 +9,22 @@ config.clusters.forEach(function(v, i, a){
 
 function selectProducer(message) {
   var target = "default";
-  if ("topic_list" == config.load_balancer) {
-    config.clusters.forEach(function(v, i, a) {
-      if (v.topic_list.indexOf(message.topic) > -1) {
-        target = v.name;
-      }
-    });
-  } else {
-    config.clusters.forEach(function(v, i, a) {
-      if (v.idc_list.indexOf(message.idc) > -1) {
-        target = v.name;
-      }
-    });
-  }
+  try {
+    if ("topic_list" == config.load_balancer) {
+      config.clusters.forEach(function(v, i, a) {
+        var topic = message.product + "_" + message.service + "_topic";
+        if (v.topic_list.indexOf(topic) > -1) {
+          target = v.name;
+        }
+      });
+    } else {
+      config.clusters.forEach(function(v, i, a) {
+        if (v.idc_list.indexOf(message.idc) > -1) {
+          target = v.name;
+        }
+      });
+    }
+  } catch (err) {}
   return Producers[target];
 }
 
@@ -54,23 +57,22 @@ function publish(msg) {
 }
 
 function httpPublish(req, res, next) {
-  var result = {}
+  //var result = {}
+  //result.response = publish(req.params.message);
+  //res.send(JSON.stringify(result));
 
-  result.response = publish(req.params.message);
-  res.send(JSON.stringify(result));
-
+  res.send({reponse : publish(req.params.message) });
   next();
 }
 
 var cluster = require('cluster');
-var dgram = require("dgram");
 var workers = config.workers || require('os').cpus().length;
 
 if (cluster.isMaster) {
   for (var i = 0; i < workers; i++) {
     cluster.fork();
   }
-  
+
   cluster.on('exit', function(worker, code, signal) {
     console.log('worker ' + worker.process.pid + ' died');
   });
@@ -94,7 +96,17 @@ if (cluster.isMaster) {
       console.log('Process ID: ' + process.pid + ' HTTP Server listening on %s', httpServer.url);
     });
 
+    // tcp server
+    net = require('net');
+    net.createServer(function (socket) {
+      socket.on('data', function (data) {
+        // publish
+        publish(data.toString());
+      });
+    }).listen(8089);
+
     // udp server
+    var dgram = require("dgram");
     var udpServer = dgram.createSocket(config.udp_version, function(msg, rinfo) {
       publish(msg);
     });
